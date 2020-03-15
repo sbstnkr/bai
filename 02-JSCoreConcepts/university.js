2d38d2ee34922a85ac6513265b6ec185892ecaf3
sbstnkr <44314400+sbstnkr@users.noreply.github.com>
2020-03-15 21:10:30 +0100

    Update university.js

--- 02-JSCoreConcepts/university.js
+++ 02-JSCoreConcepts/university.js
@@ -2,34 +2,31 @@ names = ['Piotr', 'Anna', 'Michał', 'Paulina', 'Mateusz', 'Magda'];
 
 class Students {
     constructor(names) {
-        // ...
+        this.names = names;
     }
 
     startWith(letter){
-        return // names (array) which starts with the letter
+        return names.filter((name) => name.startsWith(letter))
     }
 
     sort() {
-        return // names (array) in alphabetical order 
+        return names.sort();
     }
 
     get() {
-        return // names (array) in natural order
+        return names;
     }
 
     getFirst(n) {
-        return // get first 'n' names (array) 
+    		return names.slice(0, n);
     }
 }
 
 students = new Students(names);
 console.log(students.get());
 console.log(students.sort());
-console.log(students.startWith('P'));
-console.log(students.startWith('M').sort());
-console.log(students.sort().getFirst(4));
-console.log(students.startWith('M').sort().getFirst(2));
+console.log(students.startWith("P"));
+console.log(students.startWith("M").sort());
+console.log(students.getFirst(4).sort());
+console.log(students.getFirst(2).startWith("M").sort());
 console.log(students.get());
-
-
-
