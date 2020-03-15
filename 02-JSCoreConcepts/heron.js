0d0ec0bd9c7fb8416764afd2b2a673dd9a453c25
sbstnkr <44314400+sbstnkr@users.noreply.github.com>
2020-03-15 15:49:51 +0100

    Update heron.js

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -5,7 +5,7 @@ let c = 6;
 let d = () => {
     let p=(a+b+c)/2;
     let S=Math.sqrt(p*(p-a)*(p-b)*(p-c));
-    return "Pole trójkąta o bokach: ${a}, ${b} oraz ${c} wynosi ${S}";
+    return `Pole trójkąta o bokach: ${a}, ${b} oraz ${c} wynosi ${S}`;
 }
 
 console.log( d() );
