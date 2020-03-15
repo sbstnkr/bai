7c9e5d27388a60c1a65ddcfeddd16d4ec5b8b295
sbstnkr <44314400+sbstnkr@users.noreply.github.com>
2020-03-15 20:11:12 +0100

    Update person.js

--- 02-JSCoreConcepts/person.js
+++ 02-JSCoreConcepts/person.js
@@ -2,14 +2,16 @@ class Person {
 	constructor(firstName, lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName; 
-  }
+  	}
 	fullName() { 
   	return `${this.firstName} ${this.lastName.toUpperCase()} `
 	}
-  initials() {
-  return `${this.firstName[0]}.${this.lastName[0]}.`
-  }
+  	initials() {
+	  return `${this.firstName[0]}.${this.lastName[0]}.`
+ 	}
 }
+
 person = new Person("Ania", "Kowalska");
+
 console.log(person.fullName())
 console.log(person.initials())
