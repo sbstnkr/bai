0ffa21d87d1cc4ac56bcaabde51d7c0ee6f4194c
sbstnkr <44314400+sbstnkr@users.noreply.github.com>
2020-03-15 16:33:08 +0100

    Update cars.js

--- 02-JSCoreConcepts/cars.js
+++ 02-JSCoreConcepts/cars.js
@@ -1,5 +1,11 @@
 function isCar(car1, car2, car3) {
-    // return 
+    if (car1 == true && car2 == true && car3 == true) {
+        return true
+    } else if (car1 == false && car2 == false && car3 == false) {
+        return true
+    } else {
+        return false
+    }
 }
 
 console.log(isCar(true, true, true));
@@ -7,4 +13,3 @@ console.log(isCar(false, false, false));
 console.log(isCar(true, true, false));
 console.log(isCar(false, false, true));
 console.log(isCar(true, false, true));
-
