684e84075f1bb9dc763472c1218f6b72e98c40fc
sbstnkr <44314400+sbstnkr@users.noreply.github.com>
2020-03-23 22:48:35 +0100

    Update loop.js

--- 02-JSCoreConcepts/loop.js
+++ 02-JSCoreConcepts/loop.js
@@ -1,4 +1,10 @@
-console.log(2);
-console.log(6);
-console.log(10);
-console.log(14);
+let i = 2;
+
+while (i < 18) {
+  console.log(i);
+  i += 4;
+}
+
+for (i = 2; i < 18; i += 4) {
+  console.log(i);
+}
